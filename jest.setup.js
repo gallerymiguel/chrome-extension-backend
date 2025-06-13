d78f3acd1382611c27520f32f40754bfda654f97
Miguel <galleriesproductions@gmail.com>
2025-06-12 23:24:29 -0500

    test: set JWT_SECRET once in Jest global setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,2 +1,5 @@
 /* eslint-env jest */
 jest.mock("stripe");
+
+// Add ONE safe, dummy secret for every test file:
+process.env.JWT_SECRET = "devjwt1234567890devjwt1234567890";   // ≥32 chars keeps libs happy
